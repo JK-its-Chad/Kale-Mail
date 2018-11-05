7e0aa17e3066e252c67b3b81986acef0c0d37576
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-11-05 15:35:03 -0500

    Kale Plants and Grows
    seedbox
    seeds
    plots

---
+++ Assets/Scripts/PlotPlanting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotPlanting : MonoBehaviour {
+
+    public GameObject kale;
+    bool plotTaken = false;
+    GameObject plant;
+
+    private void Update()
+    {
+        if(plant.transform.position != transform.position && plotTaken == true)
+        {
+            plotTaken = false;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12 && plotTaken == false)
+        {
+            Destroy(other);
+            plant = Instantiate(kale, transform.position, Quaternion.identity) as GameObject;
+            plotTaken = true;
+        }
+    }
+}
