f084420f61b55c6f5d968aed03360ca8eaaeeea8
Raus <bloxxer14@gmail.com>
2018-11-19 16:16:47 -0500

    Everything

---
+++ Assets/Scripts/Tools/Marker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : Tool
+{
+	public enum Color { Red, Blue, Green };
+
+	[SerializeField]
+	private Transform cap;
+	[SerializeField]
+	private Color color = Color.Red;
+
+	public override void Pickup()
+	{
+		cap.localPosition = new Vector3(-0.1875f, 0, 0);
+	}
+
+	public override void Drop()
+	{
+		cap.localPosition = new Vector3(0.1875f, 0, 0);
+	}
+
+	public override void Begin()
+	{
+		// Paint
+	}
+}
