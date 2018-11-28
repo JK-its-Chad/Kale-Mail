7aeeab791076dda11399389a7f921c7d7b2daa2d
Raus <bloxxer14@gmail.com>
2018-11-28 15:37:03 -0500

    Stuff

--- Assets/Scripts/KalePress.cs
+++ Assets/Scripts/KalePress.cs
@@ -2,28 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KalePress : MonoBehaviour {
-
-    public GameObject kaleChips;
-    public GameObject pressTop;
-    public GameObject lever;
-    public float angle = 0;
-
-
-    int timer = 0;
-    bool pressed = false;
-
-    Vector3 lockedPosition;
+public class KalePress : MonoBehaviour
+{
+	[SerializeField]
+    private GameObject kaleChips;
+	[SerializeField]
+	public Transform press;
+	[SerializeField]
+	public Transform lever;
     
-
-	void Start ()
-    {
-        lockedPosition = lever.transform.position;
-	}
+    private int timer = 0;
+	private bool pressed = false;
 	
 	void Update ()
     {
-        lever.transform.position = lockedPosition;
+		float angle = Quaternion.Angle(lever.rotation, Quaternion.Euler(225f, 0, 0)) / 90f;
+
+		press.localPosition = new Vector3(0.05f, 0.45f - angle * 0.4f, 0);
+
+		if (press.localPosition.y <= 0.1f)
+		{
+			pressed = true;
+			if (timer >= 2)
+			{
+				pressed = false;
+			}
+			++timer;
+		}
+		else
+		{
+			timer = 0;
+		}
+
+		/*lever.transform.position = lockedPosition;
         Vector3 holder = pressTop.transform.localPosition;
         angle = Quaternion.Angle(lever.transform.rotation, Quaternion.Euler(Vector3.zero));
         if (angle >= 70)
@@ -53,15 +64,15 @@ public class KalePress : MonoBehaviour {
         if(holder.y >= .15f)
         {
             timer = 0;
-        }
-    }
+        }*/
+	}
 
     private void OnTriggerStay(Collider other)
     {
   
-        if(other.GetComponent<AllKale>())
+        if (other.GetComponent<AllKale>())
         {
-            if(pressed)
+            if (pressed)
             {
                 Destroy(other.gameObject);
                 GameObject chip = Instantiate(kaleChips, new Vector3(.9f, .815f, .4f), Quaternion.identity) as GameObject;
